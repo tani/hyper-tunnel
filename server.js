4c13dd004a4af4c7e373f206232a627f289407a8
asciian <asciian@users.noreply.github.com>
2018-02-08 21:46:46 +0900

    Add License text

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
+/* 
+ * This file is part of the noncloud.
+ * Copyright (c) 2018 TANIGUCHI Masaya.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 const WebSocket = require('ws');
 const CircularJSON = require('circular-json');
 const BodyParser = require('body-parser');
@@ -17,6 +34,10 @@ websocketserver.on('connection', (socket, { headers: { authorization } })=>{
             });
         });
     });
+});
+
+express.get('/', (request, response)=>{
+    response.redirect('https://github.com/asciian/noncloud')
 });
 
 server.listen(process.env.PORT);
