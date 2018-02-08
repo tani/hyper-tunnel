e9743a414fe418ccfd8d750d545188072618a714
asciian <asciian@users.noreply.github.com>
2018-02-08 20:46:13 +0900

    Fix  router

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const BodyParser = require('body-parser');
 const express = require('express')();
 const server = require('http').Server(express);
 const websocketserver = new WebSocket.Server({ server });
+
 websocketserver.on('connection', (socket, { headers: { authorization } })=>{
     express.use(BodyParser.raw({ type: '*/*' }));    
     socket.once('message', (applicationname)=>{
-        express.all(`/${applicationname}/`, (request, response)=>{
+        express.all(`/${applicationname}/*`, (request, response)=>{
             socket.send(CircularJSON.stringify(request)); 
             socket.once('message', (data)=>{
                 const websocketresponse = CircularJSON.parse(data);
@@ -17,4 +18,5 @@ websocketserver.on('connection', (socket, { headers: { authorization } })=>{
         });
     });
 });
-server.listen(4000);
+
+server.listen(process.env.PORT);
