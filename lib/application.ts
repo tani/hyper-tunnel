e94e5b19b22519310b9896332788340fa7e1a500
TANIGUCHI Masaya <mew@cat.ovh>
2018-02-10 13:26:27 +0900

    Check with tslint

--- lib/application.ts
+++ lib/application.ts
@@ -1,8 +1,25 @@
-import Express = require('express');
-import * as BodyParser from 'body-parser';
-import * as CircularJSON from 'circular-json';
-import { database } from './database';
-import { Message, RawMessage, RequestMessage, MessageHandler } from './message';
+/*
+ * This file is part of the noncloud.
+ * Copyright (c) 2018 TANIGUCHI Masaya.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import * as BodyParser from "body-parser";
+import * as CircularJSON from "circular-json";
+import Express = require("express");
+import { database } from "./database";
+import { IRequestMessage, Message, MessageHandler, RawMessage } from "./message";
 
 export const application = Express();
 
@@ -14,30 +31,34 @@ const applicationHandler = (request: Express.Request, response: Express.Response
     if (!database[request.params.name]) {
         notFoundHandler(request, response);
     } else {
-        const requestMessage: RequestMessage = { type: 'request', payload: request }
-        const rawMessage: RawMessage = CircularJSON.stringify(requestMessage);
-        const messageHandler: MessageHandler = (data: RawMessage) => {
-            const message: Message = CircularJSON.parse(data);
-            if (message.type === 'response') {
-                response.set(message.payload.headers);
-                response.status(message.payload.status).send(message.payload.data);
-            } else {
-                notFoundHandler(request, response);
-            }
-        };
-        database[request.params.name].send(rawMessage);
-        database[request.params.name].once('message', messageHandler);
+        {
+            const requestMessage: IRequestMessage = { type: "request", payload: request };
+            const rawMessage: RawMessage = CircularJSON.stringify(requestMessage);
+            database[request.params.name].send(rawMessage);
+        }
+        {
+            const messageHandler: MessageHandler = (rawMessage: RawMessage) => {
+                const message: Message = CircularJSON.parse(rawMessage);
+                if (message.type === "response") {
+                    response.set(message.payload.headers);
+                    response.status(message.payload.status).send(message.payload.data);
+                } else {
+                    notFoundHandler(request, response);
+                }
+            };
+            database[request.params.name].once("message", messageHandler);
+        }
     }
 };
 
-application.all('/:name/*', applicationHandler);
+application.all("/:name/*", applicationHandler);
 
-application.all('/:name', (request: Express.Request, response: Express.Response) => {
+application.all("/:name", (request: Express.Request, response: Express.Response) => {
     response.redirect(`/${request.params.name}/`);
 });
 
-application.get('/', (request: Express.Request, response: Express.Response) => {
-    response.redirect('https://github.com/asciian/noncloud');
+application.get("/", (request: Express.Request, response: Express.Response) => {
+    response.redirect("https://github.com/asciian/noncloud");
 });
 
-application.use(BodyParser.raw({ type: '*/*' }));
+application.use(BodyParser.raw({ type: "*/*" }));
--- lib/server.ts
+++ lib/server.ts
@@ -1,5 +1,22 @@
-import * as Http from 'http';
-import { application } from './application';
+/*
+ * This file is part of the noncloud.
+ * Copyright (c) 2018 TANIGUCHI Masaya.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import * as Http from "http";
+import { application } from "./application";
 export const server = Http.createServer(application);
-import './websocket';
+import "./websocket";
 server.listen(process.env.PORT);
